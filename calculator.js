3d561bda56da388c0074fbde2772c101b8a27a16
fouad8080 <fouad1563ghgg@gmail.com>
2026-04-17 09:53:34 +0100

    first Edition

--- calculator.js
+++ calculator.js
@@ -1,1 +1,21 @@
-console.log("ss")
+let display = document.getElementById('screen');
+
+function  writeinscreen(values){
+   if (!display) return;
+    display.value += values;
+}
+
+function clearscreen(){
+    console.log('das')
+    display.value=null
+}
+function calcule(){
+    
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="Error";
+    }
+
+}
